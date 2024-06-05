27940e42d91b405a0ba54a96fdf49c8670122d03
Emerson Patryck <emeps@outlook.com>
2024-06-05 17:23:20 -0300

    feat: configurar status bar

--- App.tsx
+++ App.tsx
@@ -10,14 +10,19 @@ import {
 //Components
 import { Groups } from "@screens/Groups";
 import { Loading } from "@components/Loading";
+import { StatusBar } from "react-native";
 
 export default function App() {
   const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold})
 
-
   return (
     <ThemeProvider theme={theme}>
-      { fontsLoaded? <Groups /> : <Loading/>}
+      <StatusBar 
+      barStyle={'light-content'}
+      backgroundColor={'transparent'}
+      translucent
+      />
+      { fontsLoaded ? <Groups /> : <Loading/>}
     </ThemeProvider>
   );
 }
